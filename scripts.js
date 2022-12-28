151cf5fe7e242e67debfdd6d247bd6338a338a72
Daniel King <king.daniel.thomas@gmail.com>
2022-12-28 13:23:07 +0700

    Add boilerplate js comments

---
+++ scripts.js
@@ -0,0 +1,57 @@
+ /* ========================= \
+|     GLOBAL DECLARATIONS      |
+ \ ========================= */
+
+// INITIALIZE VARIABLES
+
+
+
+// GETS + SELECTORS
+
+let numkeys = document.querySelectorAll('.numkey');
+
+// FUNCTIONS
+
+
+ /* ========================== \
+|  END OF GLOBAL DECLARATIONS   |
+ \ ========================== */
+
+
+
+ /* ========================= \
+|           NUMKEYS            |
+ \ ========================= */
+
+
+
+
+ /* ========================== \
+|         END OF NUMKEYS        |
+ \ ========================== */
+
+
+
+
+ /* ========================= \
+|          OPERATORS           |
+ \ =========================* /
+
+// OPERATE
+// 1. Calculate
+// 2. Round to two decimals
+// 3. If big number, show exponent?
+
+
+// AC Clears Memory and Clears screen .io
+
+// Equals
+// Operates, Displays, and CLEARS MEMORY.
+
+// +, -, *, /
+// Operates, displays results, keeps current value in memory.
+
+
+ /* ========================== \
+|        END OF OPERATORS       |
+ \ ========================== */
